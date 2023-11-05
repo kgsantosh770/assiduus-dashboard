6a14a87e223e0097d4762cf794ca180d7ceb7444
Santosh <s.kunga.ganeshbabu@accenture.com>
2023-11-05 20:07:18 +0530

    App Drawer comp and routes added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,64 @@
+import { AccountBalance, AttachMoney, Contacts, Dashboard, Description, Person } from "@mui/icons-material";
+import AppDrawer from "./components/AppDrawer/AppDrawer";
+import { ThemeProvider, createTheme } from "@mui/material";
+
 function App() {
+  const appDrawerList = [
+    {
+      text: 'Dashboard',
+      path: '/',
+      icon: <Dashboard />
+    },
+    {
+      text: 'Accounts',
+      path: '/accounts',
+      icon: <AccountBalance />
+    },
+    {
+      text: 'Payroll',
+      path: '/payroll',
+      icon: <AttachMoney />
+    },
+    {
+      text: 'Reports',
+      path: '/reports',
+      icon: <Description />
+    },
+    {
+      text: 'Advisor',
+      path: '/advisor',
+      icon: <Person />
+    },
+    {
+      text: 'Contacts',
+      path: '/contacts',
+      icon: <Contacts />
+    }
+  ]
+
+  const muiTheme = createTheme({
+    typography: {
+      fontFamily: 'Poppins',
+      text: {
+        primary: '#000',
+      },
+    },
+    components: {
+      MuiIcon: {
+        styleOverrides: {
+          root: {
+            color: '#000',
+          },
+        },
+      },
+    },
+  })
   return (
-    <div className="App">
-    </div>
+    <ThemeProvider theme={muiTheme}>
+      <div className="App">
+        <AppDrawer list={appDrawerList} />
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/AppDrawer/AppDrawer.js
@@ -0,0 +1,68 @@
+import Drawer from '@mui/material/Drawer';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import LogoWithText from '../../assets/assiduus-logo-withtext.png';
+import { NavLink } from "react-router-dom";
+import './AppDrawer.css';
+
+const AppDrawer = (props) => {
+    const listButtonStyle = {
+        '&&': {
+            px: '1.5rem',
+            py: '.75rem',
+        }
+    }
+
+    const toolBarStyle = {
+        '&&': {
+            marginBottom: '1rem',
+            px: '1.5rem',
+        }
+    }
+
+    const listIconStyle = {
+        '&&': {
+            minWidth: 'unset',
+            paddingRight: '1rem',
+            color: 'black',
+        }
+    }
+
+    return (
+        <Drawer
+            variant='permanent'
+            anchor='left'
+        >
+            <Toolbar sx={toolBarStyle}>
+                <img src={LogoWithText} alt="logo" height={'36px'} />
+            </Toolbar>
+            <List>
+                {
+                    props.list.map((listItem) =>
+                        <ListItem key={listItem.text} disablePadding>
+                            <ListItemButton
+                                sx={listButtonStyle}
+                                component={NavLink}
+                                to={listItem.path}
+                                className={({ isActive }) => isActive ? 'active' : ''}
+                            >
+                                <ListItemIcon
+                                    sx={listIconStyle}
+                                >
+                                    {listItem.icon}
+                                </ListItemIcon>
+                                <ListItemText data-testid={listItem.text + 'Text'} primary={listItem.text} />
+                            </ListItemButton>
+                        </ListItem>
+                    )
+                }
+            </List>
+        </Drawer>
+    )
+}
+
+export default AppDrawer
